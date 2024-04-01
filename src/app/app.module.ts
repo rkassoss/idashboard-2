27adda37940941d89772975bbcd469608f337c3b
Ran Kassovitz <kassoran@gmail.com>
2024-04-01 10:29:48 -0400

    data bind objectId

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { TravelComponent } from './travel/travel.component';
     BrowserModule,
     AppRoutingModule,
     ScriptLoaderModule,
-    QlikWrapperModule
+    QlikWrapperModule,
+    FormsModule
   ],
   providers: [
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
+// Options for the dropdown
+options = [
+  { value: 'fEAaZ', display: 'Option 1' },
+  { value: 'Xbapgf', display: 'Option 2' },
+  { value: 'ANKqynt', display: 'Option 3' }
+];
+
+// Property to hold the selected value
+selectedValue = this.options[0].value; // Default to the first option
+
+// Function to handle selection change
+onSelectionChange(): void {
+  console.log("Selected value changed to:", this.selectedValue);
+}
 
   constructor() { }
 
